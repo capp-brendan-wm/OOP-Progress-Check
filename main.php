1eb3b1b7447fa8fbef7288fcdcd651297ded2fd6
Brendan Capp <brendan.capp@west-mec.org>
2016-09-22 15:22:19 -0700

    This is the completion of the OOP Progress Check #2.

--- main.php
+++ main.php
@@ -16,12 +16,28 @@
     echo "This game is a " . $CSGO -> get_genre () . ".<br />";
     echo "This game is played on a " . $CSGO -> get_platform () . " platform" . ".<br />";
     echo "This game's cost is " . $CSGO -> get_price () . ".<br />";
+    if (gamestatistics::game_validation($id)) {
+        echo "This game code is valid";
+        echo "<br />";
+    }
+    echo $lrating -> rating();echo "<br />";
+    echo $lrating -> date(); echo "<br />";
+    echo $cguns -> guns (); echo "<br />";
+    echo $storage -> storage_req (); echo "<br />";
     echo "<br />";
+
     $LoL = new gamedetails ("League of Legends", "moba", "PC", "standard");
     echo "This game is " . $LoL -> get_game () . ".<br />";
     echo "This game is a " . $LoL -> get_genre () . ".<br />";
     echo "This game is played on a " . $LoL -> get_platform () . " platform" . ".<br />";
     echo "This game's cost is " . $LoL -> get_price () . ".<br />";
-
+    if (gamestatistics::game_validation($id)) {
+        echo "This game code is valid";
+        echo "<br />";
+    }
+    echo $crating -> rating (); echo "<br />";
+    echo $crating -> date(); echo "<br />";
+    echo $lcharacters -> characters (); echo "<br />";
+    echo $storage -> storage_req ();
 ?>
 </html>
--- secondary.php
+++ secondary.php
@@ -39,8 +39,103 @@
                 $this -> game = $game;
             }
             else if ($game == "Counter Strike Global Offensive" || "League of Legends") {
-                gamedetails :: set_game ($gamename);
+                gamedetails :: set_game ($game);
             }
         }
     }
+
+    class gamestatistics extends gameupdates {
+        public static $game_code = 10;
+
+        public static function game_validation ($id) {
+            if (strlen($id) >= self::$game_code) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+    }
+
+    $id = "1234567891";
+
+    if (gamestatistics::game_validation($id)) {
+        echo "This game code is valid";
+    }
+
+    abstract class gamerating extends gamedetails {
+        public $name;
+        public $rating;
+
+        public function name () {
+            return $this -> name . "gave the game a rating of" . $this -> rating;
+        }
+
+        abstract public function date ();
+    }
+
+    class leaguerating extends gamerating {
+        public function rating () {
+            return parent::name ();
+        }
+
+        public function date () {
+            return "The game was rated on 3/6/2016";
+        }
+    }
+
+     class counterrating extends gamerating {
+        public function rating () {
+            return parent::name ();
+        }
+
+        public function date () {
+            return "The game was rated on 3/10/2016";
+        }
+    }
+
+    $lrating = new leaguerating ("League of Legends", "moba", "PC", "standard");
+    $lrating -> name = "Ryan ";
+    $lrating -> rating = " 10/10";
+
+    $crating = new counterrating ("Counter Strike Global Offensive", "shooter", "PC", "cheap");
+    $crating -> name = "Merp ";
+    $crating -> rating = " 10/10";
+
+    class leaguecharacters {
+        public $character_1 = "Lucian";
+        public $character_2 = "Master Yi";
+        public $character_3 = "Katarina";
+
+        function characters () {
+            foreach ($this as $character => $object) {
+                print "$character => $object\n";
+            }
+        }
+    }
+
+    $lcharacters = new leaguecharacters ();
+
+    class counterguns {
+        public $gun_1 = "Assault";
+        public $gun_2 = "Submachine";
+        public $gun_3 = "Sniper";
+
+        function guns () {
+            foreach ($this as $gun => $weapon) {
+                print "$gun => $weapon\n";
+            }
+        }
+    }
+
+    $cguns = new counterguns ();
+
+    final class storage {
+        public function storage_req () {
+            echo "There storage requirements are very large";
+        }
+    }
+
+    $storage = new storage ();
 ?>
